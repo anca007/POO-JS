00791cddec01849f5ec85c33c210ab36141454b3
stropee <stropee@campus-eni.fr>
2025-09-11 14:53:14 +0200

    MOD1DEMO1

---
+++ MOD1/DEMO1/Test.js
@@ -0,0 +1,10 @@
+import {Client} from "./Client.js";
+import {Voiture} from "./Voiture.js";
+
+
+let client  = new Client();
+console.log(client);
+let v1 = new Voiture();
+console.log(v1)
+let v2 = null
+console.log(v2)
